f930a8da80fcbb9ea3bfc30dbf217d71060a322f
Antot <Anton_Chervatyuk@bk.ru>
2022-10-10 22:30:37 +0300

    Задача 13

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿ Console.Clear();
+
+ Console.WriteLine("Ведите число:");
+ int num = int.Parse(Console.ReadLine()!);
+
+ int a = num;
+ int digits = 0; // кол-во цифр
+
+ while (a > 0)
+ {
+     digits++;
+     a = a / 10;
+ }
+ if (digits < 3)
+ {
+     Console.WriteLine("Третьей цифры нет!!!");
+ }
+ else
+ {
+     int i = 0;
+     while (i < digits - 3)
+     {
+         num = num / 10;
+         i++;
+     }
+     num = num % 10;
+     Console.WriteLine($"Третья цифра: {num}");
+ }
