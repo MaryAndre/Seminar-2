40ef583e3edeaff36d57ec595a609ef24b1f3613
Antot <Anton_Chervatyuk@bk.ru>
2022-10-10 22:14:54 +0300

    задача10

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Console.Clear();
+
+// link1:
+// Console.WriteLine("Ведите трёхзначное число:");
+// int num = int.Parse(Console.ReadLine()!);
+
+// int a = num;
+// int digits = 0;
+
+
+// while (a > 0)
+// {
+//     digits++;
+//     a = a / 10;
+// }
+// if (digits < 3 || digits > 3)
+// {
+//     Console.WriteLine("Введено не трёхзначное число! Введите заново!");
+//     goto link1;
+// }
+// else
+// {
+//     int numA = (num / 10) % 10;
+//     Console.WriteLine($"Вторая цифра из числа {num}, цифра: {numA}");
+// }
