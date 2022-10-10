266d6f53c2914a03069e85ecfcc2e7a54cad1c4b
Antot <Anton_Chervatyuk@bk.ru>
2022-10-10 22:38:08 +0300

    код к задаче 15 работает

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+
+Console.WriteLine("Выходной ли день недели?:");
+link1:
+Console.WriteLine("Введите цифру дня недели от 1 до 7:");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num == 6 || num == 7)
+{
+    Console.WriteLine($"да! {num} - выходной");
+}
+else if (num < 1 || num > 7)
+{
+    Console.WriteLine("В неделе 7 дней! Введите цифру дня недели заново!");
+    goto link1;
+}
+else
+{
+    Console.WriteLine($"нет! {num} - рабочий");
+}
